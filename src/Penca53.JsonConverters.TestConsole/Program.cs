2b152542f3a7647b46c7167107c94dd2875d4ce3
Penca53 <pencapencator@gmail.com>
2021-07-30 19:14:15 +0200

    General project structure refactor Initial setup for nuget

---
+++ src/Penca53.JsonConverters.TestConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Penca53.JsonConverters.TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
